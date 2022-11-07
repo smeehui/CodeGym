723f65910ced10d991f655c9435c07653fe6e76b
smeehui <nguyenvanquanghuy99@gmail.com>
2022-11-07 16:07:33 +0700

    Money converter

---
+++ Module_2/USDToVND/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nhập vào số tiền USD cần đổi");
+        Scanner sc = new Scanner(System.in);
+        double usd = sc.nextDouble();
+        Locale usLocale = new Locale("en", "US");
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(usLocale);
+        String usdString = currencyFormatter.format(usd);
+        System.out.println(usdString +" bằng " + convertToVnd(usd));
+    }
+
+    private static String convertToVnd(double usd) {
+        Locale viLocale = new Locale("vi", "VN");
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(viLocale);
+        return currencyFormatter.format(usd * 23000);
+    }
+}
