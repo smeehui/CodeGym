9df7ddb05d2f9857fd48506504f8e35682a42019
smeehui <nguyenvanquanghuy99@gmail.com>
2022-11-07 10:14:07 +0700

    Java RectangleArea

---
+++ Module_2/RectanglArea/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Diện tích hình chữ nhật là "+rectangleArea(4,6));
+    }
+
+    private static int rectangleArea(int a, int b) {
+        return a*b;
+    }
+}
