0ff9bace05420ce3009e844599767a999c0d4ce2
smeehui <nguyenvanquanghuy99@gmail.com>
2022-11-16 19:49:00 +0700

    Java MovablePoint class

---
+++ Module_2/ClassAndObject/MovablePoint/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Point point2D = new Point(1,2);
+        System.out.println(point2D);
+        MovablePoint point1 = new MovablePoint(1,3,2,2);
+        System.out.println(point1);
+        MovablePoint point2 = point1.move();
+        System.out.println(point2);
+        MovablePoint point3 = point2.move();
+        System.out.println(point3);
+    }
+}
