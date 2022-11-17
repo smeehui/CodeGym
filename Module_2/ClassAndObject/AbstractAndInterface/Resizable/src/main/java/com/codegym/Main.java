18dd2bdd86905c4601f2b2c9691bd98a0e6a3140
smeehui <nguyenvanquanghuy99@gmail.com>
2022-11-17 14:51:13 +0700

    Java Circle Comparator Abstract and Interface

---
+++ Module_2/ClassAndObject/AbstractAndInterface/Resizable/src/main/java/com/codegym/Main.java
@@ -0,0 +1,23 @@
+package com.codegym;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(5);
+        Circle circle2 = new Circle(3);
+        Rectangle rectangle1 = new Rectangle(3,4);
+        Rectangle rectangle2 = new Rectangle(7,2);
+        Shape[] shapes = {circle1, circle2, rectangle1, rectangle2};
+
+        System.out.println("Before resize");
+        for(Shape shape: shapes){
+            System.out.println(shape);
+        }
+        for (Shape shape: shapes){
+                shape.resize();
+        }
+        System.out.println("After resize");
+        for(Shape shape: shapes){
+            System.out.println(shape);
+        }
+    }
+}
