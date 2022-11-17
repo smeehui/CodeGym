915c40e03ef87947fc5e1e673fd1bfe37340dbef
smeehui <nguyenvanquanghuy99@gmail.com>
2022-11-17 15:09:46 +0700

    Java Colorable Interface

---
+++ Module_2/ClassAndObject/AbstractAndInterface/Colorable/src/main/java/com/codegym/Main.java
@@ -0,0 +1,18 @@
+package com.codegym;
+
+import colorable.Colorable;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(5);
+        Circle circle2 = new Circle(3);
+        Rectangle rectangle1 = new Rectangle(3,4);
+        Rectangle rectangle2 = new Rectangle(7,2);
+        Square square = new Square(5,"red",true);
+        Shape[] shapes = {circle1, circle2, rectangle1, rectangle2,square};
+        for(Shape shape : shapes){
+            System.out.println(shape.getArea());
+            if(shape instanceof Colorable) System.out.println(((Colorable) shape).howToColor());
+        }
+    }
+}
---
+++ Module_2/ClassAndObject/AbstractAndInterface/Colorable/src/main/java/com/codegym/Square.java
@@ -0,0 +1,21 @@
+package com.codegym;
+
+import colorable.Colorable;
+
+public class Square extends Rectangle implements Colorable {
+    public Square() {
+
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(double side, String color, boolean isFilled) {
+        super(side, side, color, isFilled);
+    }
+
+    public String howToColor() {
+        return "Color all four side";
+    }
+}
