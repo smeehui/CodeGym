367a5c286dd6c41f006f4b5b1fce0c5df8e39e56
smeehui <nguyenvanquanghuy99@gmail.com>
2022-11-18 17:05:17 +0700

    Java Test Driven Programming FindAbsolute

---
+++ Module_2/ClassAndObject/TestDrivenProgramming/absolute-number-calculator/src/main/java/org/example/AbsoluteNumberCalculator.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number) {
+        if(number < 0) return  -number;
+        return number;
+    }
+}
