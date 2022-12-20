2fe084b73dee17c447dba4929f504bb8ea7c4dfa
smeenguyen <nguyenvanquanghuy99@gmail.com>
2022-12-20 09:45:20 +0700

    Java Odd Even Thread

---
+++ Module_2/Threading/exercises/OddEvenPrint/Main.java
@@ -0,0 +1,17 @@
+package OddEvenPrint;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main started");
+        Thread odd = new Thread(new OddThread());
+        Thread even = new Thread(new EvenThread());
+        odd.start();
+        try {
+            odd.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        even.start();
+        System.out.println("Main ended");
+    }
+}
