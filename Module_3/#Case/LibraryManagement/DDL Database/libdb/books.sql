7ed32b7b113dba01210051eedea08923bada504e
smeenguyen <nguyenvanquanghuy99@gmail.com>
2023-01-08 18:53:02 +0700

    Case Export Books data

---
+++ Module_3/#Case/LibraryManagement/DDL Database/libdb/books.sql
@@ -0,0 +1,14 @@
+create table libdb.books
+(
+    id           bigint       not null,
+    isbn         varchar(20)  null,
+    title        varchar(255) null,
+    author       varchar(255) null,
+    subject      varchar(50)  null,
+    language     varchar(50)  null,
+    dateAdded    datetime     null,
+    dateModified datetime     null,
+    available    bit          not null,
+    deleted      bit          null
+);
+
